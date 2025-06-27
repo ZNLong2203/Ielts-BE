f17da957f12d4dd7cfa3eb63c7d6245a90aa741a
No Name <93629090+fnhg1ab0@users.noreply.github.com>
2025-06-27 13:46:57 +0700

    add support lib to project

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -1,10 +1,29 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseController } from './database/database.controller';
+import { DatabaseModule } from './database/database.module';
+import { DatabaseService } from './database/database.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { MailModule } from './mail/mail.module';
+import { UtilsModule } from './utils/utils.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    PrismaModule,
+    DatabaseModule,
+    UsersModule,
+    AuthModule,
+    MailModule,
+    UtilsModule,
+  ],
+  controllers: [AppController, DatabaseController],
+  providers: [AppService, DatabaseService],
 })
 export class AppModule {}
